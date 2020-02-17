cc8faa823c5b834fffe4d61efb55513931f7da25
Juan Carlos OMB <56810855+JC-OMB@users.noreply.github.com>
2020-02-17 17:13:43 -0600

    Update Triangulo.java

--- Triangulo.java
+++ Triangulo.java
@@ -1,18 +1,18 @@
 /*Juan Carlos Ortiz de  Montellano Bochelen
   Triangulo.java
-  Eeste programa recibira 3 datos de un usurario "numericos" y en base a estos identificara que tipo de triangulo es el correspondiente */
+  Este programa recibira 3 datos de un usuario "numericos" y en base a estos identificara que tipo de triangulo es el correspondiente */
 import java.util.Scanner;
 public class Triangulo{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Bienvenido a 'Identifica tu triangulo' \nintroduzca los valores de los lados de su triangulo"); //con \n puedes brincar lineas
-        //estos son los valores dado por el usuario que sera guardado para mas tarde
+        //Estos son los valores dado por el usuario que sera guardado para mas tarde
         int X; 
         int Y;
         int Z;
         //En esta seccion de codigo se analizan los datos que no da el usuario, y no permite avanzar si el valor que nos da es menor o igual a 0
 
-        //primer lado
+        //Primer lado
             System.out.println("Introduzca su primer lado");
             do{
                  System.out.println("Ingrese un numero mayor a 0");
@@ -20,7 +20,7 @@ public class Triangulo{
             }
              while (X<=0);
         
-            //segundo lado
+            //Segundo lado
             System.out.println("Introduzca su segundo lado");
             do{
                 System.out.println("Ingrese un numero mayor a 0");
@@ -28,7 +28,7 @@ public class Triangulo{
             }
             while (Y<=0);
 
-            //tercer lado 
+            //Tercer lado 
             System.out.println("Introduzca su tercer lado ");
             do{
                 System.out.println("Ingrese un numero mayor a 0");
@@ -43,7 +43,7 @@ public class Triangulo{
             }
             //Esta seccion se encarga de determinar si con las medidas dadas por el usuario el triangulo es isoceles.
             else{
-                if (X == Y || X == Z || Y==Z) { //esta parte identifica si dos de los datos que nos dio el usuario son iguales
+                if (X == Y || X == Z || Y==Z) { //Esta parte identifica si dos de los datos que nos dio el usuario son iguales
                     if ((X==Y)&&((X+Y)>Z)) { //Analiza los dos datos iguales y comprueba que sean mayores al tercero 
                         System.out.println("Su triangulo es Isoceles"); 
                     }
@@ -62,15 +62,15 @@ public class Triangulo{
                     }
                 } 
                 else{
-                    if (((X>Y) && (X>Z)) &&((Y+Z)>X)) { //analiza los tres datos diferentes dados por el usuario  y  comprueba el mas grande de ellos, despues de esto revisa si los otros dos sumados son mas grandes que este, en caso contrario el triangulo es falso
+                    if (((X>Y) && (X>Z)) &&((Y+Z)>X)) { //Analiza los tres datos diferentes dados por el usuario  y  comprueba el mas grande de ellos, despues de esto revisa si los otros dos sumados son mas grandes que este, en caso contrario el triangulo es falso
                         System.out.println("Su triangulo es Escaleno");
                     }
                     else{
-                        if (((Y>X) && (Y>Z)) &&((X+Z)>Y)) { //analiza los tres datos diferentes dados por el usuario  y  comprueba el mas grande de ellos, despues de esto revisa si los otros dos sumados son mas grandes que este, en caso contrario el triangulo es falso
+                        if (((Y>X) && (Y>Z)) &&((X+Z)>Y)) { //Analiza los tres datos diferentes dados por el usuario  y  comprueba el mas grande de ellos, despues de esto revisa si los otros dos sumados son mas grandes que este, en caso contrario el triangulo es falso
                             System.out.println("Su triangulo es Escaleno");
                         }
                         else{
-                            if (((Z>X) && (Z>Y)) &&((X+Y)>Z)) { //analiza los tres datos diferentes dados por el usuario  y  comprueba el mas grande de ellos, despues de esto revisa si los otros dos sumados son mas grandes que este, en caso contrario el triangulo es falso
+                            if (((Z>X) && (Z>Y)) &&((X+Y)>Z)) { //Analiza los tres datos diferentes dados por el usuario  y  comprueba el mas grande de ellos, despues de esto revisa si los otros dos sumados son mas grandes que este, en caso contrario el triangulo es falso
                                 System.out.println("Su triangulo es Escaleno");
                             }
                             else{
